31485c5e4a31072fcd0d7e9fbefdd8f357fdd7ec
mavkrdof <34178589+mavkrdof@users.noreply.github.com>
2024-10-16 19:51:19 +1100

    Update main.py
    Add VLC media player integration, start working on async video playing

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from dotenv import load_dotenv
 from googleapiclient.discovery import build
 import os
 import yt_dlp
+import vlc
+import asyncio
 
 
-def main(video_folder: str = None, query: str | None = None) -> str:
+def main(video_folder: str = None, query: str | None = None, input_mode: str = 'cmd') -> str:
     if video_folder is None:
         video_folder = input(
             'Enter video folder path (C:\\Users\\USERFOLDER\\Downloads): '
             )
     if query is None:
-        query = input('Enter search query: ')
+        if input_mode == 'cmd':
+            query = input('Enter search query: ')
     youtube = init_youtube()
     search_results = search(query, youtube)
-    result = choose_result(search_results)
-    print(result)
+    if input_mode == 'cmd':
+        result = choose_result_cmd(search_results)
     video_path = download_video(
         result['value'],
         result['name'],
         video_folder=video_folder
         )
-    video_path = ''
+    if input_mode == 'cmd':
+        play_video_vlc(video_path)
     return video_path
 
 
@@ -59,7 +63,8 @@ def search(query, youtube):
 
     return search_results
 
-def choose_result(search_results, display=True) -> dict:
+
+def choose_result_cmd(search_results, display=True) -> dict:
     if display:
         print('Choose a result: ')
         for count, value in enumerate(search_results):
@@ -69,13 +74,40 @@ def choose_result(search_results, display=True) -> dict:
 
 
 def download_video(video_url: str, video_name: str, video_folder: str) -> str:
-    file_path = os.path.join(video_folder, f'{video_name}.mp4')
+    file_name_sanitized = yt_dlp.utils.sanitize_filename(video_name + '.mp4')
+    file_path = os.path.join(video_folder, file_name_sanitized)
     options = {
         'outtmpl': file_path,
     }
     with yt_dlp.YoutubeDL(options) as youtube_dl:
         youtube_dl.download(video_url)
     return file_path
+
+
+def play_video_open(video_path: str):
+    os.startfile(video_path)
+
+
+def play_video_vlc(video_path: str, audio_only: bool = False):
+    vlc_instance = vlc.Instance()
+    vlc_player = vlc_instance.media_player_new(video_path)
+    # hide the vlc window if audio_only is True
+    if audio_only:
+        vlc_player.add_option('--aout=opensles')
+    asyncio.create_task(player_play(vlc_player))
+    if input('Press enter to exit') == '':
+        vlc_player.stop()
+
+
+def player_play(vlc_player):
+    # play the video
+    vlc_player.play()
+    # wait for the player to load
+    while not vlc_player.is_playing():
+        pass
+    # wait for the player to finish
+    while vlc_player.is_playing():
+        pass
 
 
 if __name__ == '__main__':

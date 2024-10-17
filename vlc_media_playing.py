006ecd498e41aed66b2ef605dd061e9cd1ea9ac4
mavkrdof <34178589+mavkrdof@users.noreply.github.com>
2024-10-17 22:51:17 +1100

    Update vlc_media_playing.py
    currently crashes with: OSError: exception: access violation writing 0x0000000000000024
    when trying to add songs to media list
    
    working on updating song queue while its being played

--- vlc_media_playing.py
+++ vlc_media_playing.py
@@ -1,25 +1,75 @@
 import vlc
 import asyncio
+import json
+import os
 
 
-def play_video_vlc(video_path: str, audio_only: bool = False):
-    
-    vlc_instance = vlc.Instance()
-    vlc_player = vlc_instance.media_player_new(video_path)
-    # hide the vlc window if audio_only is True
-    if audio_only:
-        vlc_player.add_option('--aout=opensles')
-    asyncio.create_task(player_play(vlc_player))  # TODO: crashes after one song is played
-    if input('Press enter to exit') == '':
-        vlc_player.stop()
+class Vlc_media_player:
+    def __init__(self, video_list: list[str], config_path: str):
+        self.config_file_path = config_path
+        self.vlc_instance: vlc.Instance = vlc.Instance()
+        self.vlc_player: vlc.MediaPlayer = self.vlc_instance.media_player_new('E:\\Concs\\Documents\\Coding\\Local-Music-Player\\videos\\tiny bald man.mp4')
+        self.stop_player_event = asyncio.Event()
+        self.update_volume_event = asyncio.Event()
+        self.toggle_pause_player_event = asyncio.Event()
+        self.volume: int = self.get_volume()
+        self.player_play()
 
+    def open_media_player(self, audio_only: bool = False):
+        # hide the vlc window if audio_only is True
+        if audio_only:
+            self.vlc_player.add_option('--aout=opensles')
+        self.player_play()
 
-def player_play(vlc_player):
-    # play the video
-    vlc_player.play()
-    # wait for the player to load
-    while not vlc_player.is_playing():
-        pass
-    # wait for the player to finish
-    while vlc_player.is_playing():
-        pass
+    def get_volume(self) -> int:
+        if os.path.exists(self.config_file_path):
+            with open(self.config_file_path, 'r') as file:
+                config_data = json.load(file)
+                volume = config_data['volume']
+        else:
+            with open(self.config_file_path, 'w') as file:
+                volume = 100
+                json.dump({'volume': volume}, file)
+        return volume
+
+    def cmd_vid_control(self):
+        if input('Press enter to exit') == '':
+            self.stop_player_event.set()
+
+    def player_play(self):
+        print('Starting player...')
+        # play the video
+        self.vlc_player.play()
+        # wait for the player to load
+        while not self.vlc_player.is_playing():
+            pass
+        # wait for the player to finish
+        while self.vlc_player.is_playing():
+            # stop the player
+            if self.stop_player_event.is_set():
+                self.stop_player_event.clear()
+                break
+            # update the volume
+            if self.update_volume_event.is_set():
+                self.update_volume_event.clear()
+                self.volume = self.get_volume()
+                self.vlc_player.audio_set_volume(self.volume)
+            # pause the player
+            if self.toggle_pause_player_event.is_set():
+                self.toggle_pause_player_event.clear()
+                if self.vlc_player.is_playing():
+                    self.vlc_player.set_pause(False)
+                else:
+                    self.vlc_player.set_pause(True)
+        # stop the player
+        self.vlc_player.stop()
+
+    def clear_queue(self):
+        self.vlc_player.release()
+
+    def add_videos(self, video_list: list[str]):
+        for video in video_list:
+            media = vlc.Media(video)
+        self.vlc_player.stop()
+        self.vlc_player.set_media(media)
+        self.player_play()
